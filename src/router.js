e17aeb7054bac3d835c855de37595343a520456f
Siva <carah.tyche@gmail.com>
2019-08-07 20:35:24 -0500

    Added login/logout/signup and fixed repertoire index page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Repertoires from "./views/Repertoires.vue";
+import Signup from "./views/Signup.vue";
+import Login from "./views/Login.vue";
+import Logout from "./views/Logout.vue";
 
 Vue.use(Router);
 
@@ -12,6 +16,23 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
-    }
+    },
+    {
+      path: "/repertoires",
+      name: "repertoires",
+      component: Repertoires
+    },
+    {
+      path: "/signup",
+      name: "signup",
+      component: Signup
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    ,
+    { path: "/logout", name: "logout", component: Logout }
   ]
 });
--- src/views/Repertoires.vue
+++ src/views/Repertoires.vue
@@ -1,1 +1,27 @@
-Repertoires.vue
+<template>
+  <div class="container">
+    <h1>All Repertoires</h1>
+    <div v-for="repertoire in repertoires">
+      <h2>{{ repertoire.title }}</h2>
+      <h3>{{ repertoire.genre }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      repertoires: []
+    };
+  },
+  created: function() {
+    axios.get("/api/repertoires").then(response => {
+      this.repertoires = response.data.message;
+      console.log(this.repertoires);
+    });
+  },
+  methods: {}
+};
+</script>
