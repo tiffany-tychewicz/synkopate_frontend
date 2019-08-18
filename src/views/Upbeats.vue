b20509878e913ff28840361d674f834711f97346
Siva <carah.tyche@gmail.com>
2019-08-18 17:05:03 -0500

    working on post request for file upload API

--- src/views/Upbeats.vue
+++ src/views/Upbeats.vue
@@ -27,7 +27,37 @@ export default {
   methods: {
     showFilePicker: function() {
       const client = filestack.init(process.env.VUE_APP_FILEPICKER);
-      client.picker().open();
+
+      const options = {
+        // onFileSelected: file => {
+        //   // If you throw any error in this function it will reject the file selection.
+        //   // The error message will be displayed to the user as an alert.
+        //   if (file.size > 1000 * 1000) {
+        //     throw new Error("File too big, select something smaller than 1MB");
+        //   }
+        // },
+        onUploadDone: response => {
+          console.log("onUploadDone", response);
+          const url = response.filesUploaded[0].url;
+          const title = response.filesUploaded[0].filename;
+          console.log("the url from the response is", url);
+          console.log("the title from the response is", title);
+          // var params = {
+          //   title: title,
+          //   url: url,
+          // };
+
+          // axios
+          //   .post("/api/upbeats", params)
+          //   .then(response => {
+          //     console.log("Success", response.data);
+          //     this.$router.push("/");
+          //   })
+          //   .catch(error => console.log(error.response));
+        }
+      };
+
+      client.picker(options).open();
     }
   }
 };
